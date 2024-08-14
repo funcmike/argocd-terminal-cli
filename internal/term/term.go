8e3fda2874f3ebf566cc0912391950119d4c9717
Krzysztof Majk <funcmike@atamari.pl>
2024-08-14 12:48:38 +0200

    fix bug with sending first resize with response to cursor position

--- internal/term/term.go
+++ internal/term/term.go
@@ -10,6 +10,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 const cursorGetCode string = "\u001b[6n"
@@ -60,22 +61,29 @@ func (t *terminal) Run(ctx context.Context) error {
 	stdin, stdout, _ := term.StdStreams()
 	defer stdin.Close()
 
-	startWinsize := t.getWinsize(fd)
-
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
+
+	startOp, err := t.recvOp(ctx)
+	if err != nil {
+		return fmt.Errorf("could not receive start operation: %w", err)
+	}
+
+	startWinsize := t.getWinsize(fd)
+	if err := t.sendResizeWithCursorPosition(ctx, startWinsize, startOp); err != nil {
+		return fmt.Errorf("error sending start op: %w", err)
+	}
 
 	go func() {
 		defer cancel()
 
-		// stdin is a blocking goroutine and take wait forever
-		// so just log error
+		// stdin is a blocking goroutine and can take wait forever to stop it
 		if err := t.processStdin(ctx, stdin, fd, startWinsize); err != nil {
 			log.Printf("could not process stdin: %v", err)
 		}
 	}()
 
-	return t.processStdout(ctx, stdout, startWinsize)
+	return t.processStdout(ctx, stdout, startOp)
 }
 
 func (t *terminal) processStdin(ctx context.Context, stdin io.ReadCloser, fd uintptr, startWinsize *term.Winsize) error {
@@ -117,11 +125,13 @@ func (t *terminal) processStdin(ctx context.Context, stdin io.ReadCloser, fd uin
 	}
 }
 
-func (t *terminal) processStdout(ctx context.Context, stdout io.Writer, startWinsize *term.Winsize) error {
-	var first bool
+func (t *terminal) processStdout(ctx context.Context, stdout io.Writer, startOp argocd.Operation) error {
+	if err := t.outputOperation(stdout, startOp); err != nil {
+		return fmt.Errorf("could not output start operation to stdout: %w", err)
+	}
 
 	for {
-		op, err := t.client.Recv(ctx)
+		op, err := t.recvOp(ctx)
 		if err != nil {
 			if errors.Is(err, io.EOF) {
 				return nil
@@ -133,15 +143,8 @@ func (t *terminal) processStdout(ctx context.Context, stdout io.Writer, startWin
 			return fmt.Errorf("unexpected operation: %s", op.Operation)
 		}
 
-		if first {
-			if err := t.sendResizeWithCursorPosition(ctx, startWinsize, op); err != nil {
-				return fmt.Errorf("error sending first: %w", err)
-			}
-			first = false
-		}
-
-		if _, err := fmt.Fprintf(stdout, op.Data); err != nil {
-			return fmt.Errorf("could not write to stdout: %w", err)
+		if err := t.outputOperation(stdout, op); err != nil {
+			return fmt.Errorf("could not output operation to stdout: %w", err)
 		}
 	}
 }
@@ -153,6 +156,10 @@ func (t *terminal) getWinsize(fd uintptr) *term.Winsize {
 		panic(err)
 	}
 	return size
+}
+
+func (t *terminal) recvOp(ctx context.Context) (argocd.Operation, error) {
+	return t.client.Recv(ctx)
 }
 
 func (t *terminal) sendStdin(ctx context.Context, winsize *term.Winsize, data string) error {
@@ -176,9 +183,15 @@ func (t *terminal) sendResizeWithCursorPosition(ctx context.Context, winsize *te
 	if err := t.sendResize(ctx, winsize); err != nil {
 		return fmt.Errorf("error sending resize: %w", err)
 	}
-
-	if err := t.sendStdin(ctx, winsize, fmt.Sprintf("\u001b[1;%dR", len(op.Data)-len(cursorGetCode))); err != nil {
-		return fmt.Errorf("error sending cursor position: %w", err)
+	if strings.HasSuffix(op.Data, cursorGetCode) {
+		if err := t.sendStdin(ctx, winsize, fmt.Sprintf("\u001b[1;%dR", len(op.Data)-len(cursorGetCode))); err != nil {
+			return fmt.Errorf("error sending cursor position: %w", err)
+		}
 	}
 	return nil
+}
+
+func (t *terminal) outputOperation(writer io.Writer, op argocd.Operation) error {
+	_, err := fmt.Fprintf(writer, op.Data)
+	return err
 }
