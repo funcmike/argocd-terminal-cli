ed1badef707bdf9732cdca2119f5eeae51e85a82
Krzysztof Majk <funcmike@atamari.pl>
2024-08-14 12:46:13 +0200

    add build all archs

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -eo pipefail
+
+mkdir -p build
+
+# osx
+export GOOS=darwin GOARCH=amd64
+go build -o build/atc-macos-$GOARCH cmd/atc/main.go
+export GOOS=darwin GOARCH=arm64
+go build -o build/atc-macos-$GOARCH cmd/atc/main.go
+
+# linux
+export GOOS=linux GOARCH=amd64
+go build -o build/atc-$GOOS-$GOARCH cmd/atc/main.go
+export GOOS=linux GOARCH=arm64
+go build -o build/atc-$GOOS-$GOARCH cmd/atc/main.go
+
+# windows
+export GOOS=windows GOARCH=amd64
+go build -o build/atc-$GOOS-$GOARCH cmd/atc/main.go
+export GOOS=windows GOARCH=arm64
+go build -o build/atc-$GOOS-$GOARCH cmd/atc/main.go
+
+unset GOOS GOARCH
+
+
+
